fe6abeb7941ca3739d8f94ca1239476c83ddcde5
daltonhart <daltonvaren@gmail.com>
2020-01-30 17:02:06 -0800

    adds adjustment to generating commit history

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // External Modules
 const Git = require('nodegit');
-
+const fs = require('fs');
 // Git.Clone(
 //   'https://github.com/DaltonHart/awesome-dotfiles.git',
 //   'repos/awesome'
@@ -8,66 +8,6 @@ const Git = require('nodegit');
 //   // Work with the repository object here.
 //   console.log(repository);
 // });
-
-// Git.Repository.open('repos/awesome')
-//   // Open the master branch.
-//   .then(function(repo) {
-//     return repo.getMasterCommit();
-//   }) // Display information about commits on master.
-//   .then(function(firstCommitOnMaster) {
-//     // Create a new history event emitter.
-//     var history = firstCommitOnMaster.history();
-
-//     // Listen for commit events from the history.
-//     history.on('commit', function(commit) {
-//       // Show the commit sha.
-//       console.log('commit ' + commit.sha());
-
-//       // Store the author object.
-//       var author = commit.author();
-
-//       // Display author information.
-//       console.log('Author:\t' + author.name() + ' <' + author.email() + '>');
-
-//       // Show the commit date.
-//       console.log('Date:\t' + commit.date());
-
-//       // Give some space and show the message.
-//       console.log('\n    ' + commit.message());
-
-//       commit.getDiff().then(diffList => {
-//         // Give some space and show the dif.
-//         console.log('----DIFF----');
-//         diffList.forEach(diff => {
-//           diff.patches().then(patches => {
-//             patches.forEach(patch => {
-//               patch.hunks().then(hunks => {
-//                 hunks.forEach(hunk => {
-//                   hunk.lines().then(lines => {
-//                     console.log(
-//                       'diff',
-//                       patch.oldFile().path(),
-//                       patch.newFile().path()
-//                     );
-//                     console.log(hunk.header().trim());
-//                     lines.forEach(line => {
-//                       console.log(
-//                         String.fromCharCode(line.origin()) +
-//                           line.content().trim()
-//                       );
-//                     });
-//                   });
-//                 });
-//               });
-//             });
-//           });
-//         });
-//       });
-//     });
-
-//     // Start emitting events.
-//     history.start();
-//   });
 
 const openRepo = async location => {
   const repo = await Git.Repository.open(location);
@@ -77,38 +17,21 @@ const openRepo = async location => {
   const history = await firstCommitOnMaster.history();
   // Listen for commit events from the history.
   history.on('commit', async commit => {
-    const log = {
-      sha: commit.sha(),
-      message: commit.message(),
-      diffList: []
-    };
-    const diffList = await commit.getDiff();
-    // Give some space and show the dif.
-    if (commit.sha() === '58d9f89c13f1f7e5c5836b969490c85dab8992f1') {
-      console.log('----DIFF----');
-      diffList.forEach(async diff => {
-        const patches = await diff.patches();
-        patches.forEach(async patch => {
-          patch.hunks().then(hunks => {
-            hunks.forEach(async hunk => {
-              const lines = await hunk.lines();
-
-              log.diffList.push({
-                oldFile: patch.oldFile().path(),
-                newFile: patch.newFile().path(),
-                header: hunk.header().trim()
-              });
-              console.log(log);
-              // lines.forEach(line => {
-              //   console.log(
-              //     String.fromCharCode(line.origin()) + line.content().trim()
-              //   );
-              // });
-            });
-          });
-        });
-      });
-    }
+    console.log('----COMMIT----');
+    const generatedCommit = await generateCommit(commit);
+    //
+    fs.writeFile(
+      'output.json',
+      JSON.stringify(generatedCommit),
+      'utf8',
+      function(err) {
+        if (err) {
+          console.log('An error occured while writing JSON Object to File.');
+          return console.log(err);
+        }
+        console.log('JSON file has been saved.');
+      }
+    );
   });
 
   // Start emitting events.
@@ -116,3 +39,31 @@ const openRepo = async location => {
 };
 
 openRepo('repos/awesome');
+
+const generateCommit = async commit => {
+  const log = {
+    sha: commit.sha(),
+    message: commit.message(),
+    diffList: []
+  };
+  //
+  const diffList = await commit.getDiff();
+  diffList.forEach(async diff => {
+    const patches = await diff.patches();
+    patches.forEach(async patch => {
+      const hunks = await patch.hunks();
+      hunks.forEach(async hunk => {
+        const lines = await hunk.lines();
+        log.diffList.push({
+          oldFile: patch.oldFile().path(),
+          newFile: patch.newFile().path(),
+          header: hunk.header().trim(),
+          lines: lines.map(
+            line => String.fromCharCode(line.origin()) + line.content().trim()
+          )
+        });
+      });
+    });
+  });
+  return Promise.resolve(log);
+};
