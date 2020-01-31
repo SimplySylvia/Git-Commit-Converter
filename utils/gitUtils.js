3b981b5f3238dab40ec39f2ed1d45e53457e1895
daltonhart <daltonvaren@gmail.com>
2020-01-30 20:14:25 -0800

    refactors git utilities into own file

---
+++ utils/gitUtils.js
@@ -0,0 +1,87 @@
+// External Modules
+const Git = require('nodegit');
+const fs = require('fs');
+
+const getRepo = (url, name) => {
+  Git.Clone(url, `repos/${name}`).then(repository => {
+    // Work with the repository object here.
+    console.log(repository);
+  });
+};
+
+const compileCommits = async location => {
+  const repo = await Git.Repository.open(location);
+  const firstCommitOnMaster = await repo.getMasterCommit();
+  const history = await firstCommitOnMaster.history();
+  history.on('commit', async commit => {
+    const generatedCommit = await generateCommit(commit);
+    //
+    fs.readFile(`../${location}/output.json`, (err, data) => {
+      if (err) {
+        console.log(err);
+        fs.writeFile(
+          `../${location}/output.json`,
+          JSON.stringify({ name: location }, null, 2),
+          'utf8',
+          err => {
+            if (err) console.log(err);
+          }
+        );
+        data = JSON.stringify({ name: location });
+      }
+
+      const json = JSON.parse(data);
+      json[commit.sha()] = generatedCommit;
+      fs.writeFile(
+        `../${location}/output.json`,
+        JSON.stringify(json, null, 2),
+        'utf8',
+        err => {
+          if (err) {
+            console.log('An error occured while writing JSON Object to File.');
+            return console.log(err);
+          }
+          console.log('JSON file has been saved.');
+        }
+      );
+    });
+  });
+
+  history.start();
+};
+
+const generateCommit = async commit => {
+  const log = {
+    sha: commit.sha(),
+    message: commit.message(),
+    diffList: []
+  };
+  //
+  const diffList = await commit.getDiff();
+  for (let i = 0; i < diffList.length; i++) {
+    const diff = diffList[i];
+    const patches = await diff.patches();
+    for (let j = 0; j < patches.length; j++) {
+      const patch = patches[j];
+      const hunks = await patch.hunks();
+      hunks.forEach(async hunk => {
+        const lines = await hunk.lines();
+        log.diffList.push({
+          oldFile: patch.oldFile().path(),
+          newFile: patch.newFile().path(),
+          header: hunk.header().trim(),
+          lines: lines.map(
+            line => String.fromCharCode(line.origin()) + line.content().trim()
+          )
+        });
+      });
+    }
+  }
+
+  return Promise.resolve(log);
+};
+
+module.exports = {
+  getRepo,
+  compileCommits
+};
